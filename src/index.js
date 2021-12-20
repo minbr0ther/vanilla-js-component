fc2d3894b475038ea5976b939785c631e9410eab
minbr0ther <minbr0ther@naver.com>
2021-12-20 23:26:47 +0900

    refactor: extract component from index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,4 @@
-class Component {
-  $target;
-  $state;
-  constructor($target) {
-    this.$target = $target;
-    this.setup();
-    this.render();
-  }
-  setup() {}
-  template() {
-    return '';
-  }
-  render() {
-    this.$target.innerHTML = this.template();
-    this.setEvent();
-  }
-  setEvent() {}
-  setState(newState) {
-    this.$state = { ...this.$state, ...newState };
-    this.render();
-  }
-}
+import Component from './component/Component.js';
 
 class App extends Component {
   setup() {
