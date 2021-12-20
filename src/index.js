152c9bea2807be7516b59fff6bed58782b67c7bf
minbr0ther <minbr0ther@naver.com>
2021-12-20 23:23:19 +0900

    feat: [2-2] make abstract class from previous code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
-const $app = document.querySelector('#app');
+class Component {
+  $target;
+  $state;
+  constructor($target) {
+    this.$target = $target;
+    this.setup();
+    this.render();
+  }
+  setup() {}
+  template() {
+    return '';
+  }
+  render() {
+    this.$target.innerHTML = this.template();
+    this.setEvent();
+  }
+  setEvent() {}
+  setState(newState) {
+    this.$state = { ...this.$state, ...newState };
+    this.render();
+  }
+}
 
-let state = {
-  items: ['item1', 'item2', 'item3', 'item4'],
-};
+class App extends Component {
+  setup() {
+    this.$state = { items: ['item1', 'item2'] };
+  }
+  template() {
+    const { items } = this.$state;
+    return `
+        <ul>
+          ${items.map((item) => `<li>${item}</li>`).join('')}
+        </ul>
+        <button>추가</button>
+    `;
+  }
 
-const render = () => {
-  const { items } = state;
+  setEvent() {
+    this.$target.querySelector('button').addEventListener('click', () => {
+      const { items } = this.$state;
+      this.setState({ items: [...items, `item${items.length + 1}`] });
+    });
+  }
+}
 
-  console.log(items);
-
-  $app.innerHTML = `
-    <ul>
-      ${items.map((item) => `<li>${item}</li>`).join('')}
-    </ul>
-    <button id="append">추가</button>
-  `;
-  document.querySelector('#append').addEventListener('click', () => {
-    setState({ items: [...items, `item${items.length + 1}`] });
-  }); // state는 setState로만 변경할 수 있다.
-};
-
-const setState = (newState) => {
-  // state가 변경되면 render를 실행한다.
-  state = { ...state, ...newState };
-  render();
-};
-
-render();
-//이러한 규칙을 지켜가면서 코드를 작성한다면, 브라우저 출력되는 내용은 무조건 state에 종속되는 것이다. 즉, DOM을 직접적으로 다룰 필요가 없어진다.
+new App(document.querySelector('#app'));
