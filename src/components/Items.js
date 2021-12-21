af7f2a55f1783ebcab63da7867cb3da50df01c9c
minbr0ther <minbr0ther@naver.com>
2021-12-21 10:08:46 +0900

    feat: make delete buttons but it's not efficient

--- src/components/Items.js
+++ src/components/Items.js
@@ -6,19 +6,36 @@ export default class Items extends Component {
   }
   template() {
     const { items } = this.$state;
-    return `
+    return /*html*/ `
       <ul>
-        ${items.map((item) => `<li>${item}</li>`).join('')}
+        ${items
+          .map(
+            (item, key) => /*html*/ `
+            <li>
+                ${item}
+                <button class="deleteBtn" data-index="${key}">삭제</button>
+            </li>
+        `
+          )
+          .join('')}
       </ul>
-      <button>추가</button>
+      <button class="addBtn">추가</button>
     `;
   }
 
   setEvent() {
-    this.$target.querySelector('button').addEventListener('click', () => {
+    this.$target.querySelector('.addBtn').addEventListener('click', () => {
       const { items } = this.$state;
       this.setState({ items: [...items, `item${items.length + 1}`] });
       //setState를 하면 Component에서 state를 변경하고 render한다
     });
+
+    this.$target.querySelectorAll('.deleteBtn').forEach((deleteBtn) =>
+      deleteBtn.addEventListener('click', ({ target }) => {
+        const items = [...this.$state.items];
+        items.splice(target.dataset.index, 1);
+        this.setState({ items });
+      })
+    );
   }
 }
--- src/core/Component.js
+++ src/core/Component.js
@@ -13,6 +13,8 @@ export default class Component {
   render() {
     this.$target.innerHTML = this.template();
     this.setEvent();
+    // render을 사용할때마다 새로 이벤트를 등록한다
+    // 뿐만 아니라 반복적인 요소에 대해 각각 이벤트를 등록해야 할 땐 여간 불편한게 아니다.
   }
   setEvent() {}
   setState(newState) {
