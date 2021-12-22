b0855c0fd8e99e1d41f7818b94be0fb6b882a8b2
minbr0ther <minbr0ther@naver.com>
2021-12-22 23:19:18 +0900

    feat: deviding components 기존의 Items에 존재하던 로직을 App.js에 넘겨주고, Items, ItemAppender, ItemFilter 등은 App.js에서 넘겨주는 로직을 사용하도록 만들어야 한다.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,91 @@
 import Items from './components/Items.js';
 
-export default class App {
-  constructor($target) {
-    new Items($target);
+export default class App extends Component {
+  setup() {
+    this.$state = {
+      isFilter: 0,
+      items: [
+        { seq: 1, conteㅎnts: 'item1', active: false },
+        { seq: 2, contents: 'item2', active: true },
+      ],
+    };
+  }
+
+  template() {
+    return /*html*/ `
+      <header data-component="item-appender"></header>
+      <main data-component="items"></main>
+      <footer data-component="item-filter"></footer>
+    `;
+  }
+
+  // mounted에서 자식 컴포넌트를 마운트 해줘야 한다
+  mounted() {
+    const { filteredItems, addItem, deleteItem, toggleItem, filterItem } = this;
+    const $itemAppender = this.$target.querySelector(
+      '[data-component="item-appender"]'
+    );
+    const $items = this.$target.querySelector('[data-component="items"]');
+    const $itemFilter = this.$target.querySelector(
+      '[data-component="item-filter"]'
+    );
+
+    // 하나의 객체에서 사용하는 메소드를 넘겨줄 bind를 사용하여 this를 변경하거나,
+    // 다음과 같이 새로운 함수를 만들어줘야 한다.
+    // ex) { addItem : contents => addItem(contents)}
+    new ItemAppender($itemAppender, { addItem: addItem.bind(this) });
+    new Items($items, {
+      filteredItems,
+      deleteItem: deleteItem.bind(this),
+      toggleItem: toggleItem.bind(this),
+    });
+    new $itemFilter($itemFilter, { filterItem: filterItem.bind(this) });
+  }
+
+  get filteredItems() {
+    const { isFilter, items } = this.$state;
+
+    return items.filter(
+      ({ active }) =>
+        (isFilter === 1 && active) ||
+        (isFilter === 2 && !active) ||
+        isFilter === 0
+    );
+  }
+
+  addItem(contents) {
+    const { items } = this.$state;
+
+    const seq = Math.max(0, ...items.map((v) => v.seq)) + 1;
+    const contents = target.value;
+    const active = false;
+
+    this.setState({ items: [...items, { seq, contents, active }] });
+  }
+
+  deleteItem(seq) {
+    if (!window.confirm('정말 삭제하시겠습니까?')) return;
+
+    const items = [...this.$state.items];
+    const seq = Number(target.closest('[data-seq]').dataset.seq);
+    items.splice(
+      items.findIndex((v) => v.seq === seq),
+      1
+    );
+
+    this.setState({ items });
+  }
+
+  toggleItem(seq) {
+    const items = [...this.$state.items];
+    const index = Number(target.closest('[data-seq]').dataset.seq) - 1;
+
+    items[index].active = !items[index].active; //본인의 값의 반대를 할당한다
+
+    this.setState({ items });
+  }
+
+  filterItem(isFilter) {
+    this.setState({ isFilter });
   }
 }
-
-new App();
